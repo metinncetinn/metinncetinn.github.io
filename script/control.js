47eb0fd1e94c19030ddeb3243ebbbb932142ad49
Metin Çetin <metinncetinn96@gmail.com>
2025-04-02 09:03:26 +0300

    Clean code with dirty markup.

--- script/control.js
+++ script/control.js
@@ -1,20 +1,17 @@
-document.addEventListener('DOMContentLoaded', function () {
-    const themeButton = document.getElementById('theme-icon');
-    const body = document.body;
-  
-    // Tema simgesini değiştir ve body'ye light-mode class'ını ekle/çıkar
-    themeButton.addEventListener('click', function () {
-      body.classList.toggle('light-mode');
-  
-      if (body.classList.contains('light-mode')) {
-        themeButton.classList.remove('fa-moon');
-        themeButton.classList.add('fa-sun');
-        document.querySelector("header").style.backgroundImage = "url('../gorsel/banner.jpg')";
-      } else {
-        themeButton.classList.remove('fa-sun');
-        themeButton.classList.add('fa-moon');
-        document.querySelector("header").style.backgroundImage = "url('../gorsel/banner2.png')";
-      }
-    });
-  });
-  
+document.addEventListener('DOMContentLoaded', function() {
+	const themeButton = document.getElementById('theme-icon');
+	const body = document.body;
+	// Tema simgesini değiştir ve body'ye light-mode class'ını ekle/çıkar
+	themeButton.addEventListener('click', function() {
+		body.classList.toggle('light-mode');
+		if (body.classList.contains('light-mode')) {
+			themeButton.classList.remove('fa-moon');
+			themeButton.classList.add('fa-sun');
+			document.querySelector("header").style.backgroundImage = "url('../gorsel/banner.jpg')";
+		} else {
+			themeButton.classList.remove('fa-sun');
+			themeButton.classList.add('fa-moon');
+			document.querySelector("header").style.backgroundImage = "url('../gorsel/banner2.png')";
+		}
+	});
+});
